59bc237754ce56e8a835ea862f4efc1b1ecfee20
Sergey <borovkovsergey364@gmail.com>
2020-05-18 16:25:08 +0300

    - start stretchyHeaderView + blurEffect

---
+++ StrechyHeader+BlurEffect/StrechyHeader+BlurEffect/StretchyHeaderController.swift
@@ -0,0 +1,16 @@
+//
+//  StretchyHeaderController.swift
+//  StrechyHeader+BlurEffect
+//
+//  Created by SofiaBuslavskaya on 18/05/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class StretchyHeaderController: UICollectionViewController {
+
+
+}
