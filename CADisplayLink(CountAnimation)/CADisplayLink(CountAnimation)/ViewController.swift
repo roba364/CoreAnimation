d7722a4bd7f1dd2a8e4d450f11154f3f22237125
Sergey <borovkovsergey364@gmail.com>
2020-05-19 15:54:48 +0300

    - finished handle displayLink counting label form startValue to endValue

--- CADisplayLink(CountAnimation)/CADisplayLink(CountAnimation)/ViewController.swift
+++ CADisplayLink(CountAnimation)/CADisplayLink(CountAnimation)/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var countingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "123"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        
+        return label
+    }()
+    
+    var startValue: Double = 0
+    let endValue: Double = 100
+    
+    let animationStartDate = Date()
+    let animationDuration: Double = 1.5
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(countingLabel)
+        countingLabel.frame = view.frame
+        
+        // create my CADisplayLink here
+        let displayLink = CADisplayLink(target: self, selector: #selector(handleUpdate))
+        displayLink.add(to: .main, forMode: .default)
     }
 
-
+    @objc func handleUpdate() {
+        // time from application launched
+        let now = Date()
+        let elapsedTime = now.timeIntervalSince(animationStartDate)
+        
+        if elapsedTime > animationDuration {
+            self.countingLabel.text = "\(endValue)"
+        } else {
+            let percentage = elapsedTime / animationDuration
+            let value = startValue + percentage * (endValue - startValue)
+            self.countingLabel.text = "\(percentage)"
+        }
+        
+        
+        
+        
+//        self.countingLabel.text = "\(startValue)"
+//        startValue += 1
+//        if startValue > endValue {
+//            startValue = endValue
+//        }
+    }
 }
 
