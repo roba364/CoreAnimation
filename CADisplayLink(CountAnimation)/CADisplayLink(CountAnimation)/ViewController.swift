34b5b0f7c123de6b32c8fe5efd2b4f29472aea85
Sergey <borovkovsergey364@gmail.com>
2020-05-19 15:25:43 +0300

    - start count animation

---
+++ CADisplayLink(CountAnimation)/CADisplayLink(CountAnimation)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CADisplayLink(CountAnimation)
+//
+//  Created by SofiaBuslavskaya on 19/05/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
