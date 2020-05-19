1b7670524e99f55a299293852eb76b25780a8dce
Sergey <borovkovsergey364@gmail.com>
2020-05-19 11:23:45 +0300

    - added gradientLayer at HeaderView, added description labels at the bottom of headerview

--- StrechyHeader+BlurEffect/StrechyHeader+BlurEffect/HeaderView.swift
+++ StrechyHeader+BlurEffect/StrechyHeader+BlurEffect/HeaderView.swift
@@ -33,6 +33,9 @@ class HeaderView: UICollectionReusableView {
         
         //blur
         setupVisualEffectBlur()
+        
+        // gradient layer
+        setupGradientLayer()
     }
     
     fileprivate func setupVisualEffectBlur() {
@@ -45,6 +48,44 @@ class HeaderView: UICollectionReusableView {
         animator.fractionComplete = 0
         
         visualEffectView.fillSuperview()
+    }
+    
+    fileprivate func setupGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1]
+        
+//        layer.addSublayer(gradientLayer)
+        
+        let gradientContainerView = UIView()
+        addSubview(gradientContainerView)
+        gradientContainerView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        gradientContainerView.layer.addSublayer(gradientLayer)
+        
+        gradientLayer.frame = self.bounds
+        
+        gradientLayer.frame.origin.y -= bounds.height
+        
+        let heavyLabel = UILabel()
+        heavyLabel.text = "It's a project"
+        heavyLabel.font = .systemFont(ofSize: 24, weight: .heavy)
+        heavyLabel.textColor = .white
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Parralax, BlurEffect & GradientLayer"
+        descriptionLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        descriptionLabel.textColor = .white
+        descriptionLabel.numberOfLines = 0
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            heavyLabel, descriptionLabel
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        addSubview(stackView)
+        
+        stackView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
